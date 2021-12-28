d3742825024ffe40416eef1dbf85dfc3bbd6302a
IS404 <unableregister@gmail.com>
2021-12-28 21:43:45 +0800

    init

---
+++ src/pages/index/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <i-page>
+    <div slot="header">HEADER</div>
+
+    <div slot="footer">FOOTER</div>
+  </i-page>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style>
+.main {
+  height: 100vh;
+  line-height: 100vh;
+  text-align: center;
+}
+</style>
