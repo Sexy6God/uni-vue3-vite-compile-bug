738bda397f3dfdcc3f38e7d9bd1e5e5b0fc7bfd1
IS404 <unableregister@gmail.com>
2022-03-29 01:19:13 +0800

    update: v-model 修饰符异常

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createSSRApp } from 'vue';
 import App from './App.vue';
+import 'vue-global-api';
 
 export function createApp() {
   const app = createSSRApp(App);
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,18 +1,33 @@
 <template>
   <i-page>
-    <div slot="header">HEADER</div>
-
-    <div slot="footer">FOOTER</div>
+    <div class="main">
+      <input class="input" v-model.number="num" />
+      <button @click="log">输出log</button>
+    </div>
   </i-page>
 </template>
 
 <script setup lang="ts">
+  let num = ref(0);
+
+  function log() {
+    console.log(typeof num.value);
+    console.log('num:', num.value);
+  }
 </script>
 
 <style>
-.main {
-  height: 100vh;
-  line-height: 100vh;
-  text-align: center;
-}
+  .main {
+    padding: 20rpx;
+  }
+  .input {
+    margin: 50rpx 0;
+    width: 90%;
+    height: 80rpx;
+    color: #b2b2b2;
+    font-size: 26rpx;
+    line-height: 80rpx;
+    background: #f8f7fa;
+    border-radius: 8rpx;
+  }
 </style>
